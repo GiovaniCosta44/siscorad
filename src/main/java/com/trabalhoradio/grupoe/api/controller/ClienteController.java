bbc528335aa87637bd3ff1ca42e11976e7d35d49
Giovani Costa <giovanicosta44@gmail.com>
2022-04-22 18:25:18 -0300

    CRUD incluido

--- src/main/java/com/trabalhoradio/grupoe/api/controller/ClienteController.java
+++ src/main/java/com/trabalhoradio/grupoe/api/controller/ClienteController.java
@@ -2,24 +2,65 @@ package com.trabalhoradio.grupoe.api.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import ch.qos.logback.core.net.server.Client;
 import com.trabalhoradio.grupoe.domain.model.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.trabalhoradio.grupoe.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/clientes")
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private ClienteRepository clienteRepository;
 
-
-    @GetMapping("/clientes")
-    public List<Cliente> listar(){
-        return manager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
+    @GetMapping
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
     }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+        return clienteRepository.findById(clienteId)
+                //    .map(cliente -> ResponseEntity.ok(cliente))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+        cliente.setId(clienteId);
+        cliente = clienteRepository.save(cliente);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+        clienteRepository.deleteById(clienteId);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
---
+++ src/main/java/com/trabalhoradio/grupoe/domain/repository/ClienteRepository.java
@@ -0,0 +1,14 @@
+package com.trabalhoradio.grupoe.domain.repository;
+
+import com.trabalhoradio.grupoe.domain.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    List<Cliente> findByNome(String nome);
+    List<Cliente> findByNomeContaining(String nome);
+}
